0e3c1425c0282c7556bebc99c9d10e43f1210263
Nicholas Marshall <nmarshall23@gmail.com>
2023-08-10 14:26:19 -0400

    Set mainChannel default Volume

--- src/state/mainChannel.ts
+++ src/state/mainChannel.ts
@@ -2,28 +2,19 @@ import { useDebounceFn } from "@vueuse/core";
 import { createProjection } from '@vueuse/math'
 
 import * as Tone from "tone";
-import { computed, ref, watchEffect } from "vue";
+import { ref, watchEffect } from "vue";
 
 const volumeSliderOptions = {
     maxValue: 100,
     minValue: 0,
     step: 10,
-    innerMin: 5,
+    innerMin: 1,
     innerMax: 99,
-    projectionMax: -6,
-    projectionMix: -40
+    projectionMax: -5,
+    projectionMix: -25
 }
 const mainChannel = new Tone.Channel(-30).receive("main").toDestination();
 
-// const deciScaleExp = new Tone.Scale(-60, -20).connect(mainChannel.volume.input);
-// const deciSignalExp = new Tone.Signal(0.5).connect(deciScaleExp);
-// const scale = new Tone.Scale(10, 100).connect(deciScaleExp)
-// const volumeSignal = new Tone.Signal({
-//     value: -40,
-//     units: "decibels",
-//     maxValue: volumeRange.maxValue,
-//     minValue: volumeRange.minValue
-// }).connect(mainChannel.volume);
 
 const setVolume = useDebounceFn((n: number) => {
     console.log('MainChannel Vol ', mainChannel.volume.value)
@@ -31,13 +22,10 @@ const setVolume = useDebounceFn((n: number) => {
     mainChannel.volume.rampTo(n, 3)
   }, 1500)
 
-// const volumeRef = computed({
-//     get: () => volumeSignal.value,
-//     set: async (n: number) => await setVolume(n)
-// })
+
 
 const useProjector = createProjection([0, 100], [volumeSliderOptions.projectionMix, volumeSliderOptions.projectionMax])
-const volumeRef = ref(30)
+const volumeRef = ref(50)
 const projected = useProjector(volumeRef)
 
 watchEffect(() => {
