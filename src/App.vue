b66bbb8d57b252bc0f53b40be72601848c82eebe
Nicholas Marshall <nmarshall23@gmail.com>
2023-08-15 15:27:10 -0400

    Fixed Reset Program Button

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,14 @@
         <q-toolbar-title> Binaual Beat Meditation App </q-toolbar-title>
         <q-space />
 
-        <q-btn v-if="showResetButton" round flat icon="arrow_back" to="/">
+        <q-btn
+          v-if="showResetButton"
+          :disable="disableRestButton"
+          round
+          flat
+          icon="arrow_back"
+          to="/"
+        >
           <q-tooltip>Reset Program</q-tooltip>
         </q-btn>
       </q-toolbar>
@@ -18,23 +25,45 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { ref, watch, watchEffect } from "vue";
 import { useRoute } from "vue-router/auto";
+import { usePlaybackState } from "./state/playbackState";
 
 const showResetButton = ref(true);
+const disableRestButton = ref(false);
+
+const { isPlaying } = usePlaybackState();
 
 const route = useRoute();
 
-watch(
-  () => route.path,
-  (path) => {
-    console.log("TO %o", path);
-    if (path === "/") {
-      showResetButton.value = false;
-    } else {
-      showResetButton.value = true;
-    }
-  },
-  { immediate: true }
-);
+// watch(
+//   () => route.path,
+//   (path) => {
+//     console.log("TO %o", path);
+//     if (path === "/") {
+//       showResetButton.value = false;
+//     } else {
+//       showResetButton.value = true;
+//     }
+//   },
+//   { immediate: true }
+// );
+
+watchEffect(() => {
+  if (isPlaying.value && route.path != "/") {
+    disableRestButton.value = true;
+  }
+
+  if (!isPlaying.value && route.path != "/") {
+    disableRestButton.value = false;
+  }
+
+  if (route.path === "/") {
+    showResetButton.value = false;
+  }
+
+  if (route.path !== "/") {
+    showResetButton.value = true;
+  }
+});
 </script>
