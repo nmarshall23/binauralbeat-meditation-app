d12a766715b8d2bef57d22d81b612e805ce4fd14
Nicholas Marshall <nmarshall23@gmail.com>
2023-08-17 15:22:35 -0400

    removed unused dependencies

--- src/use/useLogger.ts
+++ src/use/useLogger.ts
@@ -1,8 +1,6 @@
 
-import {CategoryProvider, Category} from "typescript-logging-category-style";
 
-const provider = CategoryProvider.createProvider("AppProvider");
 
-export function getLogger(name: string): Category {
-  return provider.getCategory(name);
+export function getLogger(name: string) {
+  
 }
